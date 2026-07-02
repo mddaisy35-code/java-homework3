e099a6b1763d85e6b89c5e4a39a9d5aa0b3c3ff9
mddaisy35-code <mddaisy35@gmail.com>
2026-07-02 19:40:54 +0530

    java homework3

---
+++ class basicInformation.java
@@ -0,0 +1,16 @@
+public class basicInformation {
+    public static void main(String[] args) {
+		
+-------------------------------------!
+        String name = "Hari";
+        int age = 17;
+        char grade = 'B';
+        boolean passed = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Grade: " + grade);
+        System.out.println("Passed: " + passed);
+---------------------------------------!		
+    }
+}
---
+++ totalAverage.java
@@ -0,0 +1,16 @@
+public class totalAverage {
+    public static void main(String[] args) {
+System.out.println("**********************************");
+        int mark1 = 80;
+        int mark2 = 90;
+        int mark3 = 70;
+
+        int total = mark1 + mark2 + mark3;
+        int average = total / 3;
+
+        System.out.println("Total = " + total);
+        System.out.println("Average = " + average);
+System.out.println("**********************************");
+		
+    }
+}
